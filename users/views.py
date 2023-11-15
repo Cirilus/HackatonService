ea715d9b4bf832016202b1f6078f77d1b39cfc8b
barashF <108573558+barashF@users.noreply.github.com>
2023-11-16 00:07:14 +0300

    fix admin_user

--- users/views.py
+++ users/views.py
@@ -7,43 +7,18 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly, IsAuthenticated
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample, OpenApiResponse, OpenApiRequest
 from drf_spectacular.types import OpenApiTypes
+from rest_framework import viewsets
+from .models import User
 
 
 @extend_schema(
         tags=["User Views"],
         description='CRUD user',
-        request=UserSerializer,
-        responses={200: UserSerializer},
     )
-@extend_schema(tags=['Invite in team'])
-class UserView(APIView):
-
-    @permission_classes([AllowAny,])
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return JsonResponse({'result':'success'})
-    
-    @permission_classes([IsAuthenticated,])
-    def put(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return JsonResponse({'result':'success'})
-    
-    @extend_schema(
-        description='Удаление юзера',
-        parameters=[OpenApiParameter(name='id', description='id', required=True, type=int),],
-        request=OpenApiRequest(),
-        responses={200: OpenApiTypes.OBJECT, 404: OpenApiTypes.OBJECT},
-        examples=[OpenApiExample(name='Example kick from team', value={'result': 'заебца'}, response_only=True, status_codes=[200]),
-                  OpenApiExample(name='Example kick from team', value={'error': 'message'}, response_only=True, status_codes=[404]),
-        ],
-    )
-    def delete(self, request):
-        GetUser().delete_user(request.GET.get('id'))
-        return JsonResponse({'result':'success'})
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny, ]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 
