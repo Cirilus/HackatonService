43fc03b31aae5ee60bc26c4cd22fb186fe503c66
reisenbest <drgn96rd@gmail.com>
2024-03-20 20:56:27 +0300

    origin back

--- recruto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from .views import task_2
-
-urlpatterns = [
-    path('task1', views.task_1, name='task_1'),
-    path('task2/', task_2.as_view(), name='generate_code'),
-    path('auth/', include('rest_framework.urls')),
-]
